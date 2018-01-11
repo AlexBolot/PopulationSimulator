b99958fc3e3f02b71ffbea76d7814bf4d61edd9b
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2018-01-11 23:36:00 +0100

    Updating class : App, Const -> Adding JavaDoc Relates to #11

--- src/main/java/PopulationSimulator/App.java
+++ src/main/java/PopulationSimulator/App.java
@@ -10,21 +10,29 @@ import PopulationSimulator.rules.Applyable;
 import PopulationSimulator.rules.CoupleRule;
 import PopulationSimulator.rules.LifespanRule;
 import PopulationSimulator.rules.ReproductionRule;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.stream.IntStream;
+
+import static PopulationSimulator.utils.Const.randBetween;
 
 /*................................................................................................................................
  . Copyright (c)
  .
  . The App class was coded by : Alexandre BOLOT
  .
- . Last modified : 11/01/18 22:19
+ . Last modified : 11/01/18 23:25
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
+/**
+ <hr>
+ <h2>This class is the main access to the Simulator.<br>
+ Nothing more to say :)</h2>
+ */
 public class App
 {
     public static void main (String[] args)
@@ -44,11 +52,28 @@ public class App
         new SimulationController(new Population(people), rules).simulate(25);
     }
 
+    /**
+     <hr>
+     <h2>Generates a random Person with :<br>
+     - ID between 10 and 100 <br>
+     - Age = 0 <br>
+     - Gender = random from the Enum <br>
+     - Sexual Ori. = random from the Enum</h2>
+     <hr>
+     <h3>
+     Created : Alexandre Bolot 10/01 <br>
+     Modified : Alexandre Bolot 11/01
+     </h3>
+     <hr>
+
+     @return A new Person with random data (see details above)
+     */
+    @NotNull
     private static Person randPerson ()
     {
         Random random = new Random();
 
-        int ID = random.nextInt(90) + 10;
+        int ID = randBetween(10, 100);
         int age = 0;
         Gender gender = Gender.values()[random.nextInt(Gender.values().length)];
         SexualOrientation orientation = SexualOrientation.values()[random.nextInt(SexualOrientation.values().length)];
--- src/main/java/PopulationSimulator/utils/Const.java
+++ src/main/java/PopulationSimulator/utils/Const.java
@@ -7,23 +7,31 @@ import java.util.Random;
  .
  . The Const class was coded by : Alexandre BOLOT
  .
- . Last modified : 11/01/18 22:21
+ . Last modified : 11/01/18 23:28
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
+/**
+ <hr>
+ <h2>This class is the holder of all constants fields and methods that are helpfull for the whole project as utils static methods</h2>
+ */
 public class Const
 {
     private static final Random random = new Random();
 
     /**
      <hr>
-     <h2>Generates a random int between lowBound (included) and highBound (excluded)</h2>
-     <h3>randNum is in range of : [lowBound : highBound[</h3>
+     <h2>Generates a random int between 2 values</h2>
+     <hr>
+     <h3>
+     Created : Alexandre Bolot 10/01 <br>
+     Modified : Alexandre Bolot 10/01
+     </h3>
      <hr>
 
-     @param lowBound  Included lower bound of the random number
-     @param highBound Excluded higer bound of the random number
+     @param lowBound  Included, lower bound of the random number
+     @param highBound Excluded, higer bound of the random number
      @return A random int between lowBound (included) and highBound (excluded)
      */
     public static int randBetween (int lowBound, int highBound)
