AlexBolot/PopulationSimulator

commit ac2b08894747d172cf7085410845ebef8cc16d1a

Adding : EdgeTypes (Sibling, Brother, Sister, etc) Upgrading : CoupleRule takes siblings out of consideration Upgrading : ReproductionRule take number of children in consideration Moving : DotGenerator is now used from SimulationController Resolves #42 Resolves #43 Resolves #44 Resolves #45

src/main/java/PopulationSimulator/model/rules/CoupleRule.java
@@ -4,9 +4,12 @@ import CodingUtils.ArrayList8;
 import PopulationSimulator.model.entities.Person;
 import PopulationSimulator.model.enums.Gender;
 import PopulationSimulator.model.enums.SexualOrientation;
+import PopulationSimulator.model.graph.Edge;
 import PopulationSimulator.model.graph.Graph;
 import PopulationSimulator.model.graph.Node;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 import static PopulationSimulator.model.enums.EdgeType.*;
 import static PopulationSimulator.model.enums.Gender.Male;
@@ -17,7 +20,7 @@ import static PopulationSimulator.model.enums.SexualOrientation.*;
  .
  . The CoupleRule class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 11:52
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -79,16 +82,16 @@ public class CoupleRule extends SimpleRule {
         ArrayList8<Node<Person>> tmpPeople = context.getNodesContaining(Person.class).mapAndCollect(node -> (Node<Person>) node).subList(
                 node -> {
                     boolean oldEnough = node.value().data().age() >= minimumAge;
-                    boolean single = !context.getEdgesFrom(node).contains(edge -> edge.type().isSameAs(Couple));
+                    boolean single = context.getEdgesOfType(node, Couple).isEmpty();
                     return oldEnough && single;
                 });
 
         for (Node<Person> node1 : tmpPeople) {
-            if (context.getEdgesFrom(node1).contains(edge -> edge.type().isSameAs(Couple))) continue;
-
             for (Node<Person> node2 : tmpPeople) {
-                if (context.getEdgesFrom(node2).contains(edge -> edge.type().isSameAs(Couple))) continue;
 
+                Optional<Edge> optEdge = context.getEdge(node1, node2);
+
+                if (optEdge.isPresent() && optEdge.get().type().isSameAs(Family)) continue;
                 if (node1.equals(node2)) continue;
                 if (!isMatch(node1.value(), node2.value())) continue;
 

src/main/java/PopulationSimulator/App.java
@@ -4,6 +4,8 @@ import CodingUtils.ArrayList8;
 import PopulationSimulator.controllers.SimulationController;
 import PopulationSimulator.model.Sector;
 import PopulationSimulator.model.entities.Person;
+import PopulationSimulator.model.enums.EdgeType;
+import PopulationSimulator.model.enums.Gender;
 import PopulationSimulator.model.factories.PersonFactory;
 import PopulationSimulator.model.graph.Graph;
 import PopulationSimulator.model.graph.Node;
@@ -11,11 +13,9 @@ import PopulationSimulator.model.rules.Applyable;
 import PopulationSimulator.model.rules.CoupleRule;
 import PopulationSimulator.model.rules.LifespanRule;
 import PopulationSimulator.model.rules.ReproductionRule;
-import PopulationSimulator.visualizer.DotGenerator;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
-import java.util.stream.IntStream;
 
 import static PopulationSimulator.utils.Const.randBetween;
 
@@ -24,7 +24,7 @@ import static PopulationSimulator.utils.Const.randBetween;
  .
  . The App class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 07:36
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -37,12 +37,18 @@ import static PopulationSimulator.utils.Const.randBetween;
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         ArrayList8<Person> people = new ArrayList8<>();
-        IntStream.range(0, 10).forEach(i -> people.add(PersonFactory.createPerson()));
+        //IntStream.range(0, 10).forEach(i -> people.add(PersonFactory.createPerson()));
+
+        people.addAll(PersonFactory.createCouple(Gender.Male, Gender.Female));
+        people.addAll(PersonFactory.createCouple(Gender.Male, Gender.Female));
+        people.addAll(PersonFactory.createCouple(Gender.Male, Gender.Female));
+        people.addAll(PersonFactory.createCouple(Gender.Male, Gender.Female));
+        people.addAll(PersonFactory.createCouple(Gender.Male, Gender.Female));
 
         ArrayList8<Applyable> rules = new ArrayList8<>();
         rules.add(new CoupleRule(5));
         rules.add(new ReproductionRule(7));
-        rules.add(new LifespanRule(15, 3));
+        rules.add(new LifespanRule(60, 3));
 
         ArrayList8<Sector> sectors = new ArrayList8<>();
         sectors.add(new Sector(1, 3)); // 0
@@ -55,10 +61,6 @@ public class App {
         people.forEach(person -> locations.put(person, sectors.get(randBetween(0, sectors.size()))));
 
         Graph graph = new Graph(people.mapAndCollect(Node::new));
-        new SimulationController(rules, graph).simulate(20);
-
-        DotGenerator generator = new DotGenerator();
-        generator.produceFrom(graph);
-        generator.generate();
+        new SimulationController(rules, graph).simulate(50, EdgeType.Parent);
     }
 }

src/main/java/PopulationSimulator/controllers/SimulationController.java
@@ -1,16 +1,17 @@
 package PopulationSimulator.controllers;
 
 import CodingUtils.ArrayList8;
+import PopulationSimulator.model.enums.EdgeType;
 import PopulationSimulator.model.graph.Graph;
 import PopulationSimulator.model.rules.Applyable;
+import PopulationSimulator.visualizer.DotGenerator;
 import PopulationSimulator.visualizer.Logger;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import static PopulationSimulator.visualizer.Logger.LogFile.EdgesLogFile;
-import static PopulationSimulator.visualizer.Logger.LogFile.NodesLogFile;
+import java.io.IOException;
+
 import static PopulationSimulator.visualizer.Logger.clearLogs;
-import static PopulationSimulator.visualizer.Logger.log;
 import static java.lang.System.currentTimeMillis;
 
 /*................................................................................................................................
@@ -18,7 +19,7 @@ import static java.lang.System.currentTimeMillis;
  .
  . The SimulationController class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 07:36
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -29,6 +30,8 @@ public class SimulationController {
 
     private Graph context;
     private ArrayList8<Applyable> rules;
+    private DotGenerator generator;
+
     //endregion
 
     //region --------------- Constructors --------------------
@@ -44,6 +47,12 @@ public class SimulationController {
     public SimulationController(@NotNull ArrayList8<Applyable> rules, @NotNull Graph context) {
         this.rules = rules;
         this.context = context;
+
+        try {
+            this.generator = new DotGenerator();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     //endregion
 
@@ -73,7 +82,7 @@ public class SimulationController {
      *
      * @param ticks Numbers of "turns" to do before stopping the simulation
      */
-    public void simulate(int ticks) {
+    public void simulate(int ticks, EdgeType type) throws IOException, InterruptedException {
         clearLogs(Logger.LogFile.values());
 
         long millis = currentTimeMillis();
@@ -89,16 +98,8 @@ public class SimulationController {
 
             System.out.println(turnTitle);
 
-            if (!context.nodes().isEmpty()) {
-                log(turnTitle, NodesLogFile);
-                context.nodes().forEach(node -> log(node.toString(), NodesLogFile));
-                context.nodes().forEach(node -> log(node.toString(), NodesLogFile));
-            }
-
-            if (!context.edges().isEmpty()) {
-                log(turnTitle, EdgesLogFile);
-                context.edges().forEach(edge -> log(edge.toString(), EdgesLogFile));
-            }
+            generator.produceFrom(tmpGraph, type);
+            generator.generate(String.valueOf(currentTime), false);
 
             currentTime++;
         }

src/main/java/PopulationSimulator/model/enums/EdgeType.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.Contract;
  .
  . The EdgeType class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 07:36
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -17,8 +17,8 @@ public enum EdgeType {
 
     HasMet(InterPerson),
 
-    Couple(HasMet),
     Family(HasMet),
+    Couple(Family),
 
     Husband(Couple),
     Wife(Couple),
@@ -26,7 +26,14 @@ public enum EdgeType {
     Parent(Family),
     Father(Parent),
     Mother(Parent),
-    Child(Family);
+
+    Sibling(Family),
+    Brother(Sibling),
+    Sister(Sibling),
+
+    Child(Family),
+    Son(Child),
+    Daughter(Child);
 
     private EdgeType _super;
 

src/main/java/PopulationSimulator/model/rules/ReproductionRule.java
@@ -11,13 +11,14 @@ import PopulationSimulator.model.graph.Node;
 import org.jetbrains.annotations.NotNull;
 
 import static PopulationSimulator.model.enums.EdgeType.*;
+import static PopulationSimulator.model.enums.Gender.Male;
 
 /*................................................................................................................................
  . Copyright (c)
  .
  . The ReproductionRule class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 07:36
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -90,6 +91,8 @@ public class ReproductionRule extends SimpleRule {
 
             if (minimumAge != anyAge && fatherNode.value().getAge() < minimumAge) continue;
             if (minimumAge != anyAge && motherNode.value().getAge() < minimumAge) continue;
+            if (context.getNeighboorNodes(fatherNode, Parent).size() >= 3) continue;
+            if (context.getNeighboorNodes(motherNode, Parent).size() >= 3) continue;
 
             visited.add(edge.from());
             visited.add(edge.towards());
@@ -97,10 +100,17 @@ public class ReproductionRule extends SimpleRule {
             Node<Person> childNode = new Node<>(PersonFactory.createPerson());
 
             context.addNode(childNode);
+
+            for (Node sibling : context.getNeighboorNodes(fatherNode, Parent)) {
+                context.addEdge(childNode, sibling, isMale(childNode) ? Brother : Sister);
+                context.addEdge(sibling, childNode, isMale(childNode) ? Brother : Sister);
+            }
+
             context.addEdge(fatherNode, childNode, Father);
             context.addEdge(motherNode, childNode, Mother);
-            context.addEdge(childNode, fatherNode, Child);
-            context.addEdge(childNode, motherNode, Child);
+
+            context.addEdge(childNode, fatherNode, isMale(childNode) ? Son : Daughter);
+            context.addEdge(childNode, motherNode, isMale(childNode) ? Son : Daughter);
         }
 
         return context;
@@ -119,6 +129,16 @@ public class ReproductionRule extends SimpleRule {
         Gender gender2 = dataP2.gender();
 
         return gender1 != gender2;
+    }
+
+    /**
+     * Simple util method to increase code readability
+     *
+     * @param node Node<Person> of wich's gender is to test
+     * @return True if value() of [node] is Male. False otherwise.
+     */
+    private boolean isMale(@NotNull Node<Person> node) {
+        return node.value().data().gender() == Male;
     }
     //endregion
 }

src/main/java/PopulationSimulator/visualizer/DotGenerator.java
@@ -18,7 +18,7 @@ import static java.lang.Runtime.getRuntime;
  .
  . The DotGenerator class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 01:13
+ . Last modified : 14/12/18 13:17
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -29,32 +29,37 @@ public class DotGenerator {
 
     private String content = "";
 
-    public void generate() throws IOException, InterruptedException {
+    public DotGenerator() throws IOException {
+        getRuntime().exec("find " + pathToGraphs.toString() + " -mindepth 1 -delete\n");
+    }
+
+    public void generate(String postFix, boolean autoOpen) throws IOException, InterruptedException {
 
         String dotFilePath = pathToGraphs.toString();
+        String graphName = "graph" + postFix;
 
-        Files.write(Paths.get(dotFilePath, "graph.dot"), content.getBytes());
+        Files.write(Paths.get(dotFilePath, graphName + ".dot"), content.getBytes());
 
-        Process process = getRuntime().exec("dot -Tpng " + dotFilePath + "/graph.dot -o " + dotFilePath + "/graph.png");
+        Process process = getRuntime().exec("dot -Tpng " + dotFilePath + "/" + graphName + ".dot -o " + dotFilePath + "/_" + graphName + ".png");
 
         process.waitFor();
 
-        getRuntime().exec("open " + dotFilePath + "/graph.png");
+        if (autoOpen)
+            getRuntime().exec("open " + dotFilePath + "/_" + graphName + ".png");
+        else getRuntime().exec("open " + dotFilePath);
     }
 
-    public void produceFrom(Graph graph) {
+    public void produceFrom(Graph graph, EdgeType type) {
 
         StringBuilder builder = new StringBuilder("digraph PopSimulator {\n\n");
 
-        builder.append("rankdir=LR;\n");
+        //builder.append("rankdir=LR;\n");
 
         //builder.append("graph [splines=ortho]; \n");
 
         ArrayList8<Node> nodes = graph.nodes();
 
         for (Node n : nodes) {
-
-            EdgeType type = EdgeType.Parent;
 
             for (Edge edge : graph.getEdgesOfType(n, type)) {
 

commit 3f7676ef35ba6b9ac38fc33fba68b3c38bdce6eb

Fixing bug introduced in commit ac2b08894747d172cf7085410845ebef8cc16d1a -> Thougt I could simplify code, but removed a necessary check

src/main/java/PopulationSimulator/model/graph/Graph.java
@@ -15,7 +15,7 @@ import static java.util.Collections.singletonList;
  .
  . The Graph class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 11:52
+ . Last modified : 16/12/18 14:43
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -57,11 +57,6 @@ public class Graph {
         addEdge(new Edge(from, towards, type));
     }
 
-    public void addEdgeBothEnds(Node node1, Node node2, EdgeType type) {
-        addEdge(node1, node2, type);
-        addEdge(node2, node1, type);
-    }
-
     public void addEdge(Edge edge) {
         edges.addIf(edge, e -> !edges.contains(edge));
     }
@@ -78,6 +73,10 @@ public class Graph {
         nodes.remove(node);
         edges.removeAll(getEdgesFrom(node));
         edges.removeAll(getEdgesTowards(node));
+    }
+
+    public boolean hasEdge(Node from, EdgeType type) {
+        return getEdgesFrom(from).findAny(edge -> edge.type().isSameAs(type)).isPresent();
     }
 
     public boolean hasEdge(Node from, Node towards) {

src/main/java/PopulationSimulator/model/rules/CoupleRule.java
@@ -4,12 +4,9 @@ import CodingUtils.ArrayList8;
 import PopulationSimulator.model.entities.Person;
 import PopulationSimulator.model.enums.Gender;
 import PopulationSimulator.model.enums.SexualOrientation;
-import PopulationSimulator.model.graph.Edge;
 import PopulationSimulator.model.graph.Graph;
 import PopulationSimulator.model.graph.Node;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Optional;
 
 import static PopulationSimulator.model.enums.EdgeType.*;
 import static PopulationSimulator.model.enums.Gender.Male;
@@ -20,7 +17,7 @@ import static PopulationSimulator.model.enums.SexualOrientation.*;
  .
  . The CoupleRule class was coded by : Alexandre BOLOT
  .
- . Last modified : 14/12/18 13:17
+ . Last modified : 16/12/18 14:09
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -82,16 +79,17 @@ public class CoupleRule extends SimpleRule {
         ArrayList8<Node<Person>> tmpPeople = context.getNodesContaining(Person.class).mapAndCollect(node -> (Node<Person>) node).subList(
                 node -> {
                     boolean oldEnough = node.value().data().age() >= minimumAge;
-                    boolean single = context.getEdgesOfType(node, Couple).isEmpty();
+                    boolean single = !context.hasEdge(node, Couple);
                     return oldEnough && single;
                 });
 
         for (Node<Person> node1 : tmpPeople) {
+            if (context.hasEdge(node1, Couple)) continue;
+
             for (Node<Person> node2 : tmpPeople) {
+                if (context.hasEdge(node2, Couple)) continue;
 
-                Optional<Edge> optEdge = context.getEdge(node1, node2);
-
-                if (optEdge.isPresent() && optEdge.get().type().isSameAs(Family)) continue;
+                if (context.getNeighboorNodes(node1, Family, 3).contains(node2)) continue;
                 if (node1.equals(node2)) continue;
                 if (!isMatch(node1.value(), node2.value())) continue;
 